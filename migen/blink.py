c8c93237ecbda65767557a400170c0ecc4766b55
Sai Charan <saicharan00112@gmail.com>
2024-12-10 03:05:46 +0530

    test buttons on the board

--- migen/blink.py
+++ migen/blink.py
@@ -4,11 +4,14 @@ from migen.build.platforms import icebreaker
 
 # Define LED pins
 _io = [
-    ("LED1", 0, Pins("PMOD2:1"), IOStandard("LVCMOS33")),
-    ("LED2", 0, Pins("PMOD2:2"), IOStandard("LVCMOS33")),
-    ("LED3", 0, Pins("PMOD2:4"), IOStandard("LVCMOS33")),
-    ("LED4", 0, Pins("PMOD2:5"), IOStandard("LVCMOS33")),
-    ("LED5", 0, Pins("PMOD2:0"), IOStandard("LVCMOS33"))
+    ("LED1", 0, Pins("PMOD2:0"), IOStandard("LVCMOS33")),
+    ("LED2", 0, Pins("PMOD2:1"), IOStandard("LVCMOS33")),
+    ("LED3", 0, Pins("PMOD2:2"), IOStandard("LVCMOS33")),
+    ("LED4", 0, Pins("PMOD2:4"), IOStandard("LVCMOS33")),
+    ("LED5", 0, Pins("PMOD2:5"), IOStandard("LVCMOS33")),
+    ("BTN1", 0, Pins("PMOD2:7"), IOStandard("LVCMOS33")),
+    ("BTN2", 0, Pins("PMOD2:6"), IOStandard("LVCMOS33")),
+    ("BTN3", 0, Pins("PMOD2:3"), IOStandard("LVCMOS33"))
 ]
 
 class LEDTest(Module):
@@ -30,6 +33,18 @@ class LEDTest(Module):
             led2.eq(~counter[25]),
             led3.eq(counter[25]),
             led4.eq(counter[25])
+        ]
+
+class BTNTest(Module):
+    def __init__(self, platform):
+        
+        led0 = platform.request("LED0", 0)
+        pmod1 = platform.request("BTN1", 0)
+        pmod2 = platform.request("BTN2", 0)
+        pmod3 = platform.request("BTN3", 0)
+        self.sync += [
+            If(pmod1[0] & pmod2[0] & pmod3[0], led0.eq(1)),
+            If(~pmod1[0] & ~pmod2[0] & ~pmod3[0], led0.eq(0))
         ]
 
 # Use iCEBreaker platform
